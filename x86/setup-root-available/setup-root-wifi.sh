371a4c4cb6174387f0070e7cc85641e279bf306c
olbender <olbender@users.noreply.github.com>
2016-10-09 21:38:52 +0200

    Fix to x86 wifi setup.

--- x86/setup-root-available/setup-root-wifi.sh
+++ x86/setup-root-available/setup-root-wifi.sh
@@ -4,6 +4,6 @@ wpa2=( "pass1" "pass2" )
 
 for (( i = 0; i < ${#dev[@]}; i++ )); do
   for (( j = 0; j < ${#essid[@]}; j++ )); do
-    echo -e "Description='Basic WPA2 profile'\nInterface=${dev[$i]}\nConnection=wireless\nSecurity=wpa\nESSID=${essid[$j]}\nIP=dhcp\nKey=${wpa2[$j]}" > /etc/netctl/${dev[$i]}-${essid[$j]}-dhcp
+    echo -e "Description='Basic WPA2 profile'\nInterface=${dev[$i]}\nConnection=wireless\nSecurity=wpa\nESSID=${essid[$j]}\nIP=dhcp\nKey=${wpa2[$j]}" > "/etc/netctl/${dev[$i]}-${essid[$j]}-dhcp"
   done
 done
