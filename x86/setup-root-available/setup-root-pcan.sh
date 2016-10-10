d7c91bb4d463b3f0e801d82316fdf65617e207da
olbender <olbender@users.noreply.github.com>
2016-10-10 18:22:14 +0200

    Small fix to pcan in x86

--- x86/setup-root-available/setup-root-pcan.sh
+++ x86/setup-root-available/setup-root-pcan.sh
@@ -12,10 +12,10 @@ patch -p1 < peak-linux-driver-7.15.2-pcan.starttime.patch
 make NET=NO_NETDEV_SUPPORT
 make install
 
-echo -e "# pcan - automatic made entry, begin --------\n
+#echo -e "# pcan - automatic made entry, begin --------\n
 # if required add options and remove comment \n
-options pcan bitrate=${bitrate}\n
-install pcan /sbin/modprobe --ignore-install pcan\n
+#options pcan bitrate=${bitrate}\n
+#install pcan /sbin/modprobe --ignore-install pcan\n
 # pcan - automatic made entry, end ----------" > /etc/modprobe.d/pcan.conf;
 
 cd ..
