978ea157dba7f88b2e8de6b709811e4af0ba14df
olbender <olbender@users.noreply.github.com>
2016-10-13 11:46:49 +0200

    x86 pcan fix.

--- x86/setup-root-available/setup-post-root-pcan.sh
+++ x86/setup-root-available/setup-post-root-pcan.sh
@@ -1,13 +1,7 @@
 bitrate=0x1c  # truck: 0x011c
-url=https://raw.githubusercontent.com/se-research/OpenDaVINCI/master/automotive/odcantools/peak-linux-driver-7.15.2.kerneldriver
 
-wget ${url}/peak-linux-driver-7.15.2.tar.gz
-
-tar -xvzf peak-linux-driver-7.15.2.tar.gz
-rm peak-linux-driver-7.15.2.tar.gz
 cd peak-linux-driver-7.15.2
 
-wget ${url}/peak-linux-driver-7.15.2-pcan.starttime.patch
 patch -p1 < peak-linux-driver-7.15.2-pcan.starttime.patch 
 
 make NET=NO_NETDEV_SUPPORT
