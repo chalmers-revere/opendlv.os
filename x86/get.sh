cbaf95932ebe3cf3bbf87df7bbe63144a533f194
olbender <olbender@users.noreply.github.com>
2016-10-13 12:01:59 +0200

    Fix to x86 get.sh

--- x86/get.sh
+++ x86/get.sh
@@ -4,10 +4,11 @@ wget ${ROOT_URL}/x86/{install,install-conf,install-env,install-post}.sh
 
 mkdir setup-root-available
 cd setup-root-available
-wget ${ROOT_URL}/x86/setup-root-available/setup-root-{4g,desktop,pcan,router,wifi}.sh
+wget ${ROOT_URL}/x86/setup-root-available/setup-env-root-{4g,desktop,pcan,router,wifi}.sh ${ROOT_URL}/x86/setup-root-available/setup-post-root-pacn.sh
+
 cd ..
 
 mkdir setup-user-available
 cd setup-user-available
-wget ${ROOT_URL}/x86/setup-user-available/setup-user-opendlvclone.sh
+wget ${ROOT_URL}/x86/setup-user-available/setup-env-user-opendlvclone.sh
 cd ..
