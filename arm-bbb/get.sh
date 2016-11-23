ffc471e7bc00c165469ae0ef85f08ba8f0700ae7
olbender <olbender@users.noreply.github.com>
2016-11-23 11:07:51 +0100

    Updated arm-bbb get script.

--- arm-bbb/get.sh
+++ arm-bbb/get.sh
@@ -1,13 +1,14 @@
 ROOT_URL=https://raw.githubusercontent.com/chalmers-revere/opendlv.os/master
 
-wget ${ROOT_URL}/arm-bbb/{install,install-conf,install-post}.sh
+wget ${ROOT_URL}/arm-bbb/{install,install-conf,install-env,install-post}.sh
 
 mkdir setup-root-available
 cd setup-root-available
-wget ${ROOT_URL}/arm-bbb/setup-root-available/{setup-root-pru.sh,BB-BONE-PRU-00A0.dts}
+wget ${ROOT_URL}/arm-bbb/setup-root-available/setup-env-root-pru.sh ${ROOT_URL}/arm-bbb/setup-root-available/setup-post-root-pru.sh
+
 cd ..
 
 mkdir setup-user-available
 cd setup-user-available
-wget ${ROOT_URL}/arm-bbb/setup-user-available/setup-user-opendlvclone.sh
+wget ${ROOT_URL}/arm-bbb/setup-user-available/setup-env-user-opendlvclone.sh
 cd ..
