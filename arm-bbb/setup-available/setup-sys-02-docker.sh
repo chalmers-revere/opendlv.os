64ec57fa9bce34bec9b2bcaadff18fbbbb860816
Björnborg Nguyen <nguyenb@student.chalmers.se>
2016-12-23 01:58:58 +0100

    Mkdir /mnt/sdcard/docker

--- arm-bbb/setup-available/setup-sys-02-docker.sh
+++ arm-bbb/setup-available/setup-sys-02-docker.sh
@@ -8,6 +8,7 @@ for u in ${user[@]}; do
   gpasswd -a ${u} docker
 done
 
+mkdir -p /mnt/sdcard/docker/
 mkdir -p /etc/systemd/system/docker.service.d/
 echo "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -g /mnt/sdcard/docker -H fd://" > /etc/systemd/system/docker.service.d/imagelocation.conf
 
