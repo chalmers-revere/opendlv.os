3f56e998f5ebf5a7d432c020716ad4c97e840143
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2017-02-07 13:11:04 +0100

    Changed naming of the service

--- arm-bbb/setup-available/setup-post-02-adc.sh
+++ arm-bbb/setup-available/setup-post-02-adc.sh
@@ -2,8 +2,8 @@
 
 source install-conf.sh
 
-echo -e "#!/bin/bash\necho -e 'BB-ADC' > /sys/bus/platform/devices/bone_capemgr/slots" > /root/boot/adc.sh
-chmod 755 /root/boot/adc.sh
-echo -e "[Unit]\nDescription=Enables reading values from analogue DC from pins. Values can be read by 'cat /sys/bus/iio/devices/iio\:device0/in_voltage*'.\n\n[Service]\nExecStart=/root/boot/adc.sh\n\n[Install]\nWantedBy=multi-user.target " > /etc/systemd/system/adc.service 
+echo -e "#!/bin/bash\necho -e 'BB-ADC' > /sys/bus/platform/devices/bone_capemgr/slots" > /root/boot/capemgr-slots-adc.sh
+chmod 755 /root/boot/capemgr-slots-adc.sh
+echo -e "[Unit]\nDescription=Enables reading values from analogue DC from pins. Values can be read by 'cat /sys/bus/iio/devices/iio\:device0/in_voltage*'.\n\n[Service]\nExecStart=/root/boot/capemgr-slots-adc.sh\n\n[Install]\nWantedBy=multi-user.target " > /etc/systemd/system/capemgr-slots-adc.service 
 
-systemctl enable adc.service 
+systemctl enable capemgr-slots-adc.service 
