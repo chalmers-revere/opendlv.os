a042d11bd47a5538feeacda395cd6da674cbe6db
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2017-02-07 13:52:04 +0100

    Requested changes

--- arm-bbb/setup-available/setup-post-01-pru.sh
+++ arm-bbb/setup-available/setup-post-01-pru.sh
@@ -86,3 +86,10 @@ cd ../../utils/pasm_source
 source linuxbuild
 mv ../pasm /usr/bin
 chmod +x /usr/bin/pasm
+
+echo -e "#!/bin/bash\necho -e 'BB-BONE-PRU' > /sys/bus/platform/devices/bone_capemgr/slots" > /root/boot/capemgr-slots-pru.sh
+chmod 755 /root/boot/capemgr-slots-pru.sh
+echo -e "[Unit]\nDescription=Enables the PRU0, for running custom PRU programs.\n\n[Service]\nExecStart=/root/boot/capemgr-slots-pru.sh\n\n[Install]\nWantedBy=multi-user.target " > /etc/systemd/system/capemgr-slots-pru.service 
+
+systemctl enable capemgr-slots-pru.service 
+
--- arm-bbb/setup-available/setup-post-02-capemgr-slots-pru.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source install-conf.sh
-
-echo -e "#!/bin/bash\necho -e 'BB-BONE-PRU' > /sys/bus/platform/devices/bone_capemgr/slots" > /root/boot/capemgr-slots-pru.sh
-chmod 755 /root/boot/capemgr-slots-pru.sh
-echo -e "[Unit]\nDescription=Enables the PRU0, for running custom PRU programs.\n\n[Service]\nExecStart=/root/boot/capemgr-slots-pru.sh\n\n[Install]\nWantedBy=multi-user.target " > /etc/systemd/system/capemgr-slots-pru.service 
-
-systemctl enable capemgr-slots-pru.service 
