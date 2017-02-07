79a721859221012a004502160b683d668e9c0475
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2017-02-07 11:09:11 +0100

    Added the additional scripts in the get.sh

--- arm-bbb/get.sh
+++ arm-bbb/get.sh
@@ -6,4 +6,4 @@ wget ${ROOT_URL}/{install,install-conf,install-chroot,install-post}.sh
 
 mkdir setup-available
 cd setup-available
-wget  ${ROOT_URL}/setup-available/setup-post-01-pru.sh ${ROOT_URL}/setup-available/setup-post-02-docker.sh ${ROOT_URL}/setup-available/setup-user-01-opendlv.sh
+wget  ${ROOT_URL}/setup-available/setup-post-{01-pru,01-opendlv,02-docker,02-adc,02-i2c}.sh
