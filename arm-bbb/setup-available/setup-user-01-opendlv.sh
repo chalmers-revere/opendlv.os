2523340094c0204473af695ae0a52a970d19f6fc
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2017-02-07 12:59:52 +0100

    Added repo opendlv.miniature

--- arm-bbb/setup-available/setup-user-01-opendlv.sh
+++ arm-bbb/setup-available/setup-user-01-opendlv.sh
@@ -6,3 +6,4 @@ cd sdcard/repos
 git clone https://github.com/se-research/OpenDaVINCI
 git clone http://github.com/chalmers-revere/opendlv.core
 git clone http://github.com/chalmers-revere/opendlv
+git clone https://github.com/chalmers-revere/opendlv.miniature
