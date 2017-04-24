e8e70940e3e61199df743ec3909c5a1a8ecf25a7
olbender <olbender@users.noreply.github.com>
2017-04-24 21:18:03 +0200

    Four PWM channels now

--- arm-bbb/setup-available/setup-post-05-pwm.sh
+++ arm-bbb/setup-available/setup-post-05-pwm.sh
@@ -8,16 +8,26 @@ echo '/dts-v1/;
   
   part-number = "BB-PWM";
   version = "00A0";
+  
+exclusive-use =
+    "P8.13",
+    "P8.19",
+    "P9.14",
+    "P9.22",
+    "ehrpwm0A",
+    "ehrpwm1A",
+    "ehrpwm2A",
+    "ehrpwm2B";
 
   fragment@0 {
     target = <&am33xx_pinmux>;
     __overlay__ {
       pinctrl_spec: ehrpwm_pins {
         pinctrl-single,pins = <
-     /*     0x0c8 0x02 */ /* P8_36 MODE2 ehrpwm1A PWM */
-     /*     0x0cc 0x02 */ /* P8_34 MODE2 ehrpwm1B PWM */
-          0x020 0x04 /* P8_19 MODE4 ehrpwm2A PWM */
-          0x024 0x04 /* P8_13 MODE4 ehrpwm2B PWM */
+          0x024 0x04 /* P8_13, mode 4 (ehrpwm2B), pull-down, output, fast slew */
+          0x020 0x04 /* P8_19, mode 4 (ehrpwm2A), pull-down, output, fast slew */
+          0x048 0x06 /* P9_14, mode 6 (ehrpwm1A), pull-down, output, fast slew */
+          0x150 0x03 /* P9_22, mode 3 (ehrpwm0A), pull-down, output, fast slew */
         >;
       };
     };
@@ -34,16 +44,16 @@ echo '/dts-v1/;
       };
     };
   };
-  
+ 
   fragment@2 {
-    target = <&epwmss1>;
+    target = <&epwmss0>;
     __overlay__ {
       status = "okay";
     };
   };
-
+ 
   fragment@3 {
-    target = <&ehrpwm1>;
+    target = <&epwmss1>;
     __overlay__ {
       status = "okay";
     };
@@ -55,13 +65,33 @@ echo '/dts-v1/;
       status = "okay";
     };
   };
-
+    
   fragment@5 {
-    target = <&ehrpwm2>;
-    __overlay__ {
-      status = "okay";
+        target = <&ehrpwm0>;
+        __overlay__ {
+            status = "okay";
+            pinctrl-names = "default";
+            pinctrl-0 = <>; 
+        };
     };
-  };
+    
+fragment@6 {
+        target = <&ehrpwm1>;
+        __overlay__ {
+            status = "okay";
+            pinctrl-names = "default";
+            pinctrl-0 = <>; 
+        };
+    };
+    
+fragment@7 {
+        target = <&ehrpwm2>;
+        __overlay__ {
+            status = "okay";
+            pinctrl-names = "default";
+            pinctrl-0 = <>; 
+        };
+    };
 };' > BB-PWM-00A0.dts
 
 dtc -@ -O dtb -o  BB-PWM-00A0.dtbo BB-PWM-00A0.dts
