3547be7c319c98b38b5dbada63acb103bbbe0cd1
olbender <olbender@users.noreply.github.com>
2017-05-02 00:19:09 +0200

    Minor PRU setup fix.

--- arm-bbb/setup-available/setup-post-01-pru.sh
+++ arm-bbb/setup-available/setup-post-01-pru.sh
@@ -70,21 +70,21 @@ dtc -@ -O dtb -o BB-PRU-00A0.dtbo BB-PRU-00A0.dts
 
 mv BB-PRU-00A0.dtbo /lib/firmware/
 
-git clone https://github.com/beagleboard/am335x_pru_package
-cd am335x_pru_package
-mkdir /usr/include/pruss
-cp pru_sw/app_loader/include/prussdrv.h pru_sw/app_loader/include/pruss_intc_mapping.h /usr/include/pruss
-cd pru_sw/app_loader/interface/
-CROSS_COMPILE= make
+#git clone https://github.com/beagleboard/am335x_pru_package
+#cd am335x_pru_package
+#mkdir /usr/include/pruss
+#cp pru_sw/app_loader/include/prussdrv.h pru_sw/app_loader/include/pruss_intc_mapping.h /usr/include/pruss
+#cd pru_sw/app_loader/interface/
+#CROSS_COMPILE= make
 
-cd ../lib
-cp * /usr/lib
-ldconfig
+#cd ../lib
+#cp * /usr/lib
+#ldconfig
 
-cd ../../utils/pasm_source
-source linuxbuild
-mv ../pasm /usr/bin
-chmod +x /usr/bin/pasm
+#cd ../../utils/pasm_source
+#source linuxbuild
+#mv ../pasm /usr/bin
+#chmod +x /usr/bin/pasm
 
 echo -e "#!/bin/bash\necho -e 'BB-PRU' > /sys/bus/platform/devices/bone_capemgr/slots" > /root/boot/capemgr-slots-pru.sh
 chmod 755 /root/boot/capemgr-slots-pru.sh
