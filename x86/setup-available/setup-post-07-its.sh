6c711ae9b42d748f82e8a0dc0f7ea043456b9616
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2017-09-04 16:34:21 +0200

    Update setup-post-07-its.sh
    Added udp2eth functionality

--- x86/setup-available/setup-post-07-its.sh
+++ x86/setup-available/setup-post-07-its.sh
@@ -13,3 +13,14 @@ echo -e "[Unit]\nDescription=Set frequency of ITS V2V communication.\nAfter=mult
 
 systemctl enable its-setfreq.service
 
+git clone https://github.com/jandejongh/udp2eth.git
+
+cd udp2eth
+make
+cp udp2eth /usr/local/bin
+cd /root
+
+echo -e "[Unit]\nDescription=Small package converter from udp to eth frames and vice versa.\nAfter=multi-user.target its-setfreq.service\n\n[Service]\nExecStart=/usr/local/bin/udp2eth -p --device=wlp4s0 --server=127.0.0.1:4000 --client=127.0.0.1:4001\n\n[Install]\nWantedBy=multi-user.target " > /etc/systemd/system/its-udp2eth.service 
+rm -rf udp2eth
+
+systemctl enable its-udp2eth.service
