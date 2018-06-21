10ddb46eb4cfb383c5139a8d853184fe6f45ec96
olbender <olbender@users.noreply.github.com>
2018-06-21 12:11:36 +0200

    Fix

--- x86/setup-available/setup-post-09-socketcan.sh
+++ x86/setup-available/setup-post-09-socketcan.sh
@@ -5,7 +5,7 @@ bitrate=( 500000 1000000 )
 
 echo -e "#!/bin/bash\n\n" > /root/boot/socketcan-setup.sh
 for (( i = 0; i < ${#dev[@]}; i++ )); do
-  echo -e "ip link set ${dev[$i]} up type can bitrate ${bitrate[$i]}\n" > /root/boot/socketcan-setup.sh
+  echo -e "ip link set ${dev[$i]} up type can bitrate ${bitrate[$i]}\n" >> /root/boot/socketcan-setup.sh
 done
 
 chmod 755 /root/boot/socketcan-setup.sh
