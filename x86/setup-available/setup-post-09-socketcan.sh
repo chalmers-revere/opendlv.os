c1ba6fcbc48e1e971b18f7d1f58e5e76fcfd6997
olbender <olbender@users.noreply.github.com>
2018-06-21 11:57:46 +0200

    Setup socketcan

---
+++ x86/setup-available/setup-post-09-socketcan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dev=( can0 can1 )
+bitrate=( 500000 1000000 )
+
+echo -e "#!/bin/bash\n\n" > /root/boot/socketcan-setup.sh
+for (( i = 0; i < ${#dev[@]}; i++ )); do
+  echo -e "ip link set ${dev[$i]} up type can bitrate ${bitrate[$i]}\n" > /root/boot/socketcan-setup.sh
+done
+
+chmod 755 /root/boot/socketcan-setup.sh
+echo -e "[Unit]\nDescription=Setup socket can.\nAfter=multi-user.target\n\n[Service]\nExecStart=/root/boot/socketcan-setup.sh\n\n[Install]\nWantedBy=multi-user.target " > /etc/systemd/system/socketcan-setup.service 
+
+systemctl enable socketcan-setup.service
