77deaea3c6798f623c095d9256c6b7577440dd0a
olbender <olbender@users.noreply.github.com>
2018-08-29 14:25:13 +0200

    Fixed pcan file

--- x86/setup-available/setup-post-04-pcan.sh
+++ x86/setup-available/setup-post-04-pcan.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-bitrate=0x1c  # rhino: 0x011c
+bitrate=0x1c
 
-url=https://raw.githubusercontent.com/se-research/OpenDaVINCI/master/automotive/odcantools/peak-linux-driver-7.15.2.kerneldriver
+url=http://www.peak-system.com/fileadmin/media/linux/files
+name=peak-linux-driver-8.6.0
 
-wget ${url}/peak-linux-driver-7.15.2.tar.gz
+wget ${url}/${name}.tar.gz
 
-tar -xvzf peak-linux-driver-7.15.2.tar.gz
-rm peak-linux-driver-7.15.2.tar.gz
-cd peak-linux-driver-7.15.2
-
-wget ${url}/peak-linux-driver-7.15.2-pcan.starttime.patch
-patch -p1 < peak-linux-driver-7.15.2-pcan.starttime.patch 
+tar -xvzf ${name}.tar.gz
+rm ${name}.tar.gz
+cd ${name}
 
 make NET=NO_NETDEV_SUPPORT
 make install
@@ -23,4 +21,4 @@ install pcan /sbin/modprobe --ignore-install pcan\n
 # pcan - automatic made entry, end ----------" > /etc/modprobe.d/pcan.conf;
 
 cd
-rm -fr peak-linux-driver-7.15.2
+rm -fr ${name}
