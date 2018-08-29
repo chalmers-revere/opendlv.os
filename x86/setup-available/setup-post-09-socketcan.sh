5d310e9ece66012985a1f6690c8a50f1b54f0747
olbender <olbender@users.noreply.github.com>
2018-08-29 16:27:05 +0200

    Fix

--- x86/setup-available/setup-post-09-socketcan.sh
+++ x86/setup-available/setup-post-09-socketcan.sh
@@ -3,12 +3,12 @@
 dev=( can0 can1 )
 bitrate=( 500000 1000000 )
 
-echo -e "#!/bin/bash\n\n" > /root/boot/socketcan-setup.sh
+echo -e "#!/bin/bash\n\n" > /root/socketcan-setup.sh
 for (( i = 0; i < ${#dev[@]}; i++ )); do
-  echo -e "ip link set ${dev[$i]} up type can bitrate ${bitrate[$i]}\n" >> /root/boot/socketcan-setup.sh
+  echo -e "ip link set ${dev[$i]} up type can bitrate ${bitrate[$i]}\n" >> /root/socketcan-setup.sh
 done
 
-chmod 755 /root/boot/socketcan-setup.sh
-echo -e "[Unit]\nDescription=Setup socket can.\nAfter=default.target\n\n[Service]\nExecStart=/root/boot/socketcan-setup.sh\n\n[Install]\nWantedBy=default.target " > /etc/systemd/system/socketcan-setup.service 
+chmod 755 /root/socketcan-setup.sh
+echo -e "[Unit]\nDescription=Setup socket can.\nAfter=default.target\n\n[Service]\nExecStart=/root/socketcan-setup.sh\n\n[Install]\nWantedBy=default.target " > /etc/systemd/system/socketcan-setup.service 
 
 systemctl enable socketcan-setup.service
