52f6fd88788ec4b3039831d4d02ed23b360e2290
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2018-12-13 14:46:53 +0100

    Added environment

--- kiwi/arm-bbblue/install-post.sh
+++ kiwi/arm-bbblue/install-post.sh
@@ -145,6 +145,7 @@ echo -e "10.42.42.1\t kiwi.opendlv.io" >> /etc/hosts
 
 cd /root/
 curl -sSL https://raw.githubusercontent.com/chalmers-revere/opendlv.os/kiwi/kiwi/bbblue.yml 
+curl -sSL https://raw.githubusercontent.com/chalmers-revere/opendlv.os/kiwi/kiwi/.env 
 docker-compose -f bbblue.yml up -d
 
 read -p "Installation script for the beaglebone is done!"
