a6b8057795ed639d05a002aa1196069d679bb0cd
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2019-01-18 15:13:13 +0100

    Fixing -e

--- kiwi/rpi3/install-post.sh
+++ kiwi/rpi3/install-post.sh
@@ -49,22 +49,22 @@ raspi-config nonint do_camera 0
 
 #enable wireless
 echo 'network={\n    ssid="kiwi"\n    psk="opendlv-kiwi"\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf
-# echo -e 'network={\n    ssid="ChalmersVOR"\n    psk="VOR2018!"\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf
-# echo -e 'network={\n    ssid="ChalmersVOR_2G"\n    psk="VOR2018!"\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf
+# echo 'network={\n    ssid="ChalmersVOR"\n    psk="VOR2018!"\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf
+# echo 'network={\n    ssid="ChalmersVOR_2G"\n    psk="VOR2018!"\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf
 
 
 wpa_cli -i wlan0 reconfigure
 
 #dhcp
-echo -e 'authoritative;\nsubnet 10.42.42.0 netmask 255.255.255.0 {\n    range 10.42.42.10 10.42.42.50;\n    option broadcast-address 10.42.42.255;\n    option routers 10.42.42.1;\n    default-lease-time 600;\n    max-lease-time 7200;\n    option domain-name "local";\n    option domain-name-servers 8.8.8.8, 8.8.4.4;\n}' >> /etc/dhcp/dhcpd.conf
-sed -i  -e 's/option domain-name "example.org";/#option domain-name "example.org";/g' /etc/dhcp/dhcpd.conf
-sed -i  -e 's/option domain-name-servers ns1.example.org, ns2.example.org;/#option domain-name-servers ns1.example.org, ns2.example.org;/g' /etc/dhcp/dhcpd.conf
+echo 'authoritative;\nsubnet 10.42.42.0 netmask 255.255.255.0 {\n    range 10.42.42.10 10.42.42.50;\n    option broadcast-address 10.42.42.255;\n    option routers 10.42.42.1;\n    default-lease-time 600;\n    max-lease-time 7200;\n    option domain-name "local";\n    option domain-name-servers 8.8.8.8, 8.8.4.4;\n}' >> /etc/dhcp/dhcpd.conf
+sed -i  's/option domain-name "example.org";/#option domain-name "example.org";/g' /etc/dhcp/dhcpd.conf
+sed -i  's/option domain-name-servers ns1.example.org, ns2.example.org;/#option domain-name-servers ns1.example.org, ns2.example.org;/g' /etc/dhcp/dhcpd.conf
 # mutlicast
-echo -e 'ip route add 225.0.0.0/24 dev eth2' >> /etc/dhcpcd.exit-hook
-echo -e 'interface eth1\nstatic ip_address=10.42.42.1/24' >> /etc/dhcpcd.conf
-# echo -e 'interface wlan0\nstatic ip_address=10.154.84.26/24\nstatic routers=10.154.84.1\nstatic domain_name_servers=208.67.222.222' >> /etc/dhcpcd.conf
-echo -e 'auto lo\niface lo inet loopback\nallow-hotplug eth1' >> /etc/network/interfaces
-sed -i -e 's/INTERFACESv4=""/INTERFACESv4="eth1"/g' /etc/default/isc-dhcp-server
+echo 'ip route add 225.0.0.0/24 dev eth2' >> /etc/dhcpcd.exit-hook
+echo 'interface eth1\nstatic ip_address=10.42.42.1/24' >> /etc/dhcpcd.conf
+# echo 'interface wlan0\nstatic ip_address=10.154.84.26/24\nstatic routers=10.154.84.1\nstatic domain_name_servers=208.67.222.222' >> /etc/dhcpcd.conf
+echo 'auto lo\niface lo inet loopback\nallow-hotplug eth1' >> /etc/network/interfaces
+sed -i 's/INTERFACESv4=""/INTERFACESv4="eth1"/g' /etc/default/isc-dhcp-server
 
 # iface wlan0 inet static
 #     address 10.154.84.22
@@ -92,12 +92,12 @@ until [ -d /sys/class/net/eth2/ ] ; do
 done
 addr2=$(cat /sys/class/net/eth2/address)
 
-echo -e 'SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="'$addr1'", ATTR{dev_id}=="0x0", ATTR{type}=="1", KERNEL=="eth*", NAME="eth1"\nSUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="'$addr2'", ATTR{dev_id}=="0x0", ATTR{type}=="1", KERNEL=="eth*", NAME="eth2"' > /etc/udev/rules.d/70-local-persistent-net.rules
+echo 'SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="'$addr1'", ATTR{dev_id}=="0x0", ATTR{type}=="1", KERNEL=="eth*", NAME="eth1"\nSUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="'$addr2'", ATTR{dev_id}=="0x0", ATTR{type}=="1", KERNEL=="eth*", NAME="eth2"' > /etc/udev/rules.d/70-local-persistent-net.rules
 
 
 cp /run/systemd/generator.late/isc-dhcp-server.service /etc/systemd/system
-sed -i -e 's/Restart=no/Restart=on-failure\nRestartSec=5/g' /etc/systemd/system/isc-dhcp-server.service
-echo -e '\n[Install]\nWantedBy=multi-user.target' >> /etc/systemd/system/isc-dhcp-server.service
+sed -i 's/Restart=no/Restart=on-failure\nRestartSec=5/g' /etc/systemd/system/isc-dhcp-server.service
+echo '\n[Install]\nWantedBy=multi-user.target' >> /etc/systemd/system/isc-dhcp-server.service
 
 # custom ssh port
 echo "Port 8880" >> /etc/ssh/sshd_config
