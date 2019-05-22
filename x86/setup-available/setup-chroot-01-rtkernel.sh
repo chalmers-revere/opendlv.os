430ada70da9677a2d5247f62e9f8a041ca5f3e87
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2019-05-22 19:59:18 +0200

    Update setup-chroot-01-rtkernel.sh

--- x86/setup-available/setup-chroot-01-rtkernel.sh
+++ x86/setup-available/setup-chroot-01-rtkernel.sh
@@ -11,14 +11,8 @@ tar -xf linux-rt.tar.gz
 
 cd linux-rt
 
-
-#  'ABAF11C65A2970B130ABE3C479BE3E4300411886'  # Linus Torvalds
-gpg --recv-key 647F28654894E3BD457199BE38DBBDC86092693E  # Greg Kroah-Hartman
-#  '8218F88849AAC522E94CF470A5E9288C4FA415FA'  # Jan Alexander Steffens (heftig)
-#  '64254695FFF0AA4466CC19E67B96E8162A8CF5D1'  # Sebastian Andrzej Siewior
-#  '5ED9A48FC54C0A22D1D0804CEBC26CDB5A56DE73'  # Steven Rostedt
-gpg --recv-key E644E2F1D45FA0B2EAA02F33109F098506FF0B14  # Thomas Gleixner
-
+gpg --recv-key 38DBBDC86092693E
+gpg --recv-key 05641F175712FA5B
 (echo "", echo "") | MAKEFLAGS="-j$[`nproc` + 4]" makepkg -sir
 
 sudo grub-mkconfig -o /boot/grub/grub.cfg
