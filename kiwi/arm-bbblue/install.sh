16a41f0686eed732a00274e65b3e4f14ba03ab95
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2020-02-20 17:01:33 +0100

    Update install.sh

--- kiwi/arm-bbblue/install.sh
+++ kiwi/arm-bbblue/install.sh
@@ -25,32 +25,10 @@ cd /root
 cd /opt/scripts/
 git pull
 cd -
-#sed -i 's/\/sbin\/ifconfig usb1 192.168.6.2 netmask 255.255.255.252 || true/#\/sbin\/ifconfig usb1 192.168.6.2 netmask 255.255.255.252 || true/g' /opt/scripts/boot/autoconfigure_usb1.sh
-# echo 'dhclient usb1' >> /opt/scripts/boot/autoconfigure_usb1.sh
-printf '    post-up ip route add 225.0.0.0/24 dev usb0\n' >> /etc/network/interfaces
-printf 'auto usb1\niface usb1 inet dhcp\n' >> /etc/network/interfaces
-#printf 'ip route add 225.0.0.0/24 dev usb0' >> /opt/scripts/boot/autoconfigure_usb0.sh
+printf 'USB_NETWORK_CDC_DISABLED=yes\n' >> /etc/default/bb-boot
+printf 'auto lo\niface lo inet loopback\nauto usb0\niface usb0 inet dhcp\n    post-up ip route add 225.0.0.0/24 dev usb0\n' > /etc/network/interfaces
 
 
-# Create swapfile
-#fallocate -l 512M /var/swapfile
-#chmod 600 /var/swapfile
-#mkswap /var/swapfile
-#swapon /var/swapfile
-#printf "/var/swapfile\tnone\tswap\tdefaults\t0 0" >> /etc/fstab
-
-
-
-# Format sdcard
-#(echo d; echo n; echo p; echo ""; echo ""; echo ""; echo w) | fdisk /dev/mmcblk0
-#(echo y) | mkfs.ext4 /dev/mmcblk0p1
-#mkdir -p /mnt/sdcard
-#mount /dev/mmcblk0p1 /mnt/sdcard
-#printf "/dev/mmcblk0p1  /mnt/sdcard  ext4  defaults  0 2" >> /etc/fstab
-
-#mkdir -p /mnt/sdcard/users/debian
-#chown -R debian:users /mnt/sdcard/users/debian
-#su -c "ln -s /mnt/sdcard/users/debian /home/debian/sdcard" -s /bin/bash debian
 
 
 # Add unstable branch
@@ -112,10 +90,6 @@ systemctl enable ntp
 # Installing docker
 curl -sSL https://get.docker.com | sh
 usermod -aG docker debian
-systemctl stop docker.service
-#/lib/systemd/system/docker.service
-sed -i 's/\/usr\/bin\/dockerd -H fd:\/\//\/usr\/bin\/dockerd -g \/mnt\/sdcard\/docker\/ -H fd:\/\//g' /lib/systemd/system/docker.service
-systemctl daemon-reload
 systemctl start docker.service
 apt-get install -y docker-compose
 
@@ -126,29 +100,29 @@ apt-get install -y docker-compose
 #sed -i 's/#retry 60;/retry 10;/g' /etc/dhcp/dhclient.conf 
 printf 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -o usb1 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o usb0 -j MASQUERADE
 iptables -A FORWARD -i usb1 -o SoftAp0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i SoftAp0 -o usb1 -j ACCEPT
+iptables -A FORWARD -i SoftAp0 -o usb0 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o SoftAp0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i SoftAp0 -o eth0 -j ACCEPT
 
-iptables -A FORWARD -i SoftAp0 -o usb1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -o SoftAp0 -i usb1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i SoftAp0 -o usb0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -o SoftAp0 -i usb0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-iptables -A FORWARD -i SoftAp0 -o usb1 -p tcp --syn --dport 8888 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -i SoftAp0 -o usb0 -p tcp --syn --dport 8888 -m conntrack --ctstate NEW -j ACCEPT
 iptables -t nat -A PREROUTING -i SoftAp0 -p tcp --dport 8888 -j DNAT --to-destination 10.42.42.1
 
-iptables -A FORWARD -i SoftAp0 -o usb1 -p tcp --syn --dport 8080 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -i SoftAp0 -o usb0 -p tcp --syn --dport 8080 -m conntrack --ctstate NEW -j ACCEPT
 iptables -t nat -A PREROUTING -i SoftAp0 -p tcp --dport 8080 -j DNAT --to-destination 10.42.42.1
 
 iptables -A FORWARD -p tcp -d 10.42.42.1 --dport 8081 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -t nat -A PREROUTING -p tcp -i SoftAp0 --dport 80 -j DNAT --to-destination 10.42.42.1:8081
 
-iptables -A FORWARD -i SoftAp0 -o usb1 -p tcp --syn --dport 8081 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -i SoftAp0 -o usb0 -p tcp --syn --dport 8081 -m conntrack --ctstate NEW -j ACCEPT
 iptables -t nat -A PREROUTING -i SoftAp0 -p tcp --dport 8081 -j DNAT --to-destination 10.42.42.1
 
-iptables -A FORWARD -i SoftAp0 -o usb1 -p tcp --syn --dport 2200 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -i SoftAp0 -o usb0 -p tcp --syn --dport 2200 -m conntrack --ctstate NEW -j ACCEPT
 iptables -t nat -A PREROUTING -i SoftAp0 -p tcp --dport 2200 -j DNAT --to-destination 10.42.42.1
 
 iptables-save > /etc/iptables/rules.v4
