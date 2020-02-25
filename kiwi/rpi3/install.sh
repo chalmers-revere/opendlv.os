97061330297c697804253c88e53b4f68dbdfdb58
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2020-02-25 18:05:06 +0100

    Update install.sh

--- kiwi/rpi3/install.sh
+++ kiwi/rpi3/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "alias ll='ls -alF --color=auto'" >> /root/.bashrc
-echo "alias ll='ls -alF --color=auto'" >> /home/pi/.bashrc
+printf "alias ll='ls -alF --color=auto'" >> /root/.bashrc
+printf "alias ll='ls -alF --color=auto'" >> /home/pi/.bashrc
 
 timedatectl set-timezone Europe/Stockholm
 
-echo 'sv_SE.UTF-8 UTF-8' >> /etc/locale.gen
-echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+printf 'sv_SE.UTF-8 UTF-8' >> /etc/locale.gen
+printf 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 
 
 locale-gen
@@ -41,6 +41,7 @@ wget \
 gcc-6 \
 g++-6 \
 python-pip \
+docker-io \
 docker-compose \
 libusb-dev \
 isc-dhcp-server \
@@ -63,7 +64,7 @@ apt-get upgrade -y
 apt-get autoremove -y
 apt-get autoclean
 
-echo 'broadcast 10.42.42.255\nserver 127.127.1.0\nfudge 127.127.1.0 stratum 10\n' >> /etc/ntp.conf
+printf 'broadcast 10.42.42.255\nserver 127.127.1.0\nfudge 127.127.1.0 stratum 10\n' >> /etc/ntp.conf
 
 systemctl stop ntp
 ntpd -gq
@@ -77,52 +78,35 @@ raspi-config nonint do_camera 0
 
 
 #enable wireless
-echo 'country=SE' >> /etc/wpa_supplicant/wpa_supplicant.conf
-echo 'network={\n    ssid="kiwi"\n    psk="opendlv-kiwi"\n    priority=1\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf
-echo 'network={\n    ssid="IVRL"\n    psk="opendlv-ivrl"\n    priority=2\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf
-# echo 'network={\n    ssid="ChalmersVOR"\n    psk="VOR2018!"\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf
-# echo 'network={\n    ssid="ChalmersVOR_2G"\n    psk="VOR2018!"\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf
+printf 'country=SE\n' >> /etc/wpa_supplicant/wpa_supplicant.conf
+printf 'network={\n    ssid="kiwi"\n    psk="opendlv-kiwi"\n    priority=1\n}\n' >> /etc/wpa_supplicant/wpa_supplicant.conf
+printf 'network={\n    ssid="IVRL"\n    psk="opendlv-ivrl"\n    priority=2\n}\n' >> /etc/wpa_supplicant/wpa_supplicant.conf
 
 
 wpa_cli -i wlan0 reconfigure
 
 #dhcp server
-echo 'authoritative;\nsubnet 10.42.42.0 netmask 255.255.255.0 {\n    range 10.42.42.10 10.42.42.50;\n    option broadcast-address 10.42.42.255;\n    option routers 10.42.42.1;\n    default-lease-time 600;\n    max-lease-time 7200;\n    option domain-name "kiwi.opendlv.io";\n    option domain-name-servers 1.1.1.1, 1.0.0.1;\n}' >> /etc/dhcp/dhcpd.conf
+printf 'authoritative;\nsubnet 10.42.42.0 netmask 255.255.255.0 {\n    range 10.42.42.10 10.42.42.50;\n    option broadcast-address 10.42.42.255;\n    option routers 10.42.42.1;\n    default-lease-time 600;\n    max-lease-time 7200;\n    option domain-name "kiwi.opendlv.io";\n    option domain-name-servers 1.1.1.1, 1.0.0.1;\n}\n' >> /etc/dhcp/dhcpd.conf
 sed -i  's/option domain-name "example.org";/#option domain-name "example.org";/g' /etc/dhcp/dhcpd.conf
 sed -i  's/option domain-name-servers ns1.example.org, ns2.example.org;/#option domain-name-servers ns1.example.org, ns2.example.org;/g' /etc/dhcp/dhcpd.conf
 # mutlicast
-echo 'ip route add 225.0.0.0/24 dev eth1' >> /etc/dhcpcd.exit-hook
-echo 'interface eth1\nstatic ip_address=10.42.42.1/24' >> /etc/dhcpcd.conf
-# echo 'interface wlan0\nstatic ip_address=10.154.84.26/24\nstatic routers=10.154.84.1\nstatic domain_name_servers=208.67.222.222' >> /etc/dhcpcd.conf
-echo 'auto lo\niface lo inet loopback\nallow-hotplug eth1' >> /etc/network/interfaces
+printf 'ip route add 225.0.0.0/24 dev eth1\n' >> /etc/dhcpcd.exit-hook
+# static ip
+printf 'noipv6\ninterface eth1\nstatic ip_address=10.42.42.1/24\nstatic routers=10.42.42.1\nstatic domain_name-servers=1.1.1.1 1.0.0.1' >> /etc/dhcpcd.conf
+printf 'auto lo\niface lo inet loopback\nallow-hotplug eth0\nallow-hotplug eth1\n' >> /etc/network/interfaces
 sed -i 's/INTERFACESv4=""/INTERFACESv4="eth1"/g' /etc/default/isc-dhcp-server
-
-# iface wlan0 inet static
-#     address 10.154.84.22
-#     network 10.154.84.0
-#     netmask 255.255.252.0
-#     gateway 10.154.84.1
-#     broadcast 10.154.84.255
-
-#     interface wlan0
-# static ip_address=10.154.84.22/24
-# static routers=10.154.84.1
-# static domain_name_servers=208.67.222.222
-
-
-
 
 
 cp /run/systemd/generator.late/isc-dhcp-server.service /etc/systemd/system
 sed -i 's/Restart=no/Restart=on-failure\nRestartSec=5/g' /etc/systemd/system/isc-dhcp-server.service
-echo '\n[Install]\nWantedBy=multi-user.target' >> /etc/systemd/system/isc-dhcp-server.service
+printf '\n[Install]\nWantedBy=multi-user.target\n' >> /etc/systemd/system/isc-dhcp-server.service
 
 # custom ssh port
-echo "Port 2200" >> /etc/ssh/sshd_config
+printf "Port 2200\n" >> /etc/ssh/sshd_config
 
 # iptables
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-echo 1 > /proc/sys/net/ipv4/ip_forward
+printf "net.ipv4.ip_forward=1\n" >> /etc/sysctl.conf
+printf 1 > /proc/sys/net/ipv4/ip_forward
 
 iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 iptables -A FORWARD -i wlan0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
@@ -145,13 +129,9 @@ systemctl restart isc-dhcp-server
 
 
 # Installing docker
-curl -sSL https://get.docker.com | sh
+# curl -sSL https://get.docker.com | sh
 usermod -aG docker pi
-# cd /root/bbb
 cd /root
-# wget https://raw.githubusercontent.com/chalmers-revere/2018-wasp-summer-school/master/getting-started/rpi-camera-x264-viewer-kiwi.yml
-# git clone https://github.com/chalmers-revere/2018-wasp-summer-school.git
-# cd /root/2018-wasp-summer-school/getting-started
 wget -SL https://raw.githubusercontent.com/chalmers-revere/opendlv.os/kiwi/kiwi/rpi3.yml 
 docker-compose -f rpi3.yml up -d
 cd /root
